444080598f2fff09294499c23a70d15da64bde77
tostacam <edgar-tostado@hotmail.com>
2026-05-25 01:05:31 -0700

    added benchmark

--- 01_python_baseline/dgemm.py
+++ 01_python_baseline/dgemm.py
@@ -1,7 +1,8 @@
 from utilities.matrix_io import read_matrix
 from utilities.matrix_utils import zero_matrix
+from utilities.benchmark import benchmark
 
-ROWS = COLS = 4
+ROWS = COLS = 512
 
 def dgemm(A , B, C):
   for i in range(ROWS):
@@ -13,5 +14,6 @@ A = read_matrix(f"matrix_{ROWS}x{COLS}_A.bin")
 B = read_matrix(f"matrix_{ROWS}x{COLS}_B.bin")
 C = zero_matrix(ROWS, COLS)
 
-# matrix multiplication
-dgemm(A, B, C)
+elapsed = benchmark(dgemm, A, B, C)
+
+print(f"Execution time: {elapsed:.6f} seconds")
