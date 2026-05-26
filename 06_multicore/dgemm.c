ff6f6c81fc76636e9d9e6ee7be1cf3c17c2c27e9
tostacam <edgar-tostado@hotmail.com>
2026-05-26 00:39:28 -0700

    multicore init

---
+++ 06_multicore/dgemm.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utilities/matrix_utils.h"
+#include "../utilities/benchmark.h"
+
+#define MATRIX_SIZE 1024
+
+typedef struct {
+  matrix A;
+  matrix BT;
+  matrix C;
+} dgemm_args;
+
+#include <arm_neon.h> /* for MacOS M3 ARM */
+#include <omp.h>
+#define UNROLL 4
+#define BLOCKSIZE 32
+
+void do_block(int n, int si, int sj, int sk, double* A, double* BT, double* C){
+  for(size_t i = si; i < si+BLOCKSIZE; i+=2*UNROLL){
+    for(size_t j = sj; j < sj+BLOCKSIZE; ++j){
+      float64x2_t sum[UNROLL];
+      
+      for(int r = 0; r < UNROLL; ++r)
+        sum[r] = vdupq_n_f64(0.0);
+
+      float64x2_t a[UNROLL], b[UNROLL];
+      for(size_t k = sk; k < sk+BLOCKSIZE; k+=2 * UNROLL){
+        
+        for(int r = 0; r < UNROLL; ++r){
+          a[r] = vld1q_f64(&A[i*n + k + r*2]);
+          b[r] = vld1q_f64(&BT[j*n + k + r*2]);
+        }
+
+        for(int r = 0; r < UNROLL; ++r)
+          sum[r] = vfmaq_f64(sum[r], a[r], b[r]);
+      }
+
+      float64x2_t sum_vec = vdupq_n_f64(0.0);
+      for(int r = 0; r < UNROLL; ++r)
+        sum_vec = vaddq_f64(sum_vec, sum[r]);
+
+      C[i*n + j] = vgetq_lane_f64(sum_vec, 0) + vgetq_lane_f64(sum_vec, 1);
+    }
+  }
+}
+
+void dgemm(void* args_ptr){
+  dgemm_args* args = (dgemm_args*) args_ptr;
+  matrix A = args->A;
+  matrix BT = args->BT;
+  matrix C = args->C;
+
+  size_t n = A.rows;
+  #pragma omp parallel for collapse(2)
+  for(int sj = 0; sj < n; sj += BLOCKSIZE)
+    for(int si = 0; si < n; si += BLOCKSIZE)
+      for(int sk = 0; sk < n; sk += BLOCKSIZE){
+        //printf("Thread %d -> block (%d, %d, %d)]\n", omp_get_thread_num(), si, sj, sk);
+        do_block(n, si, sj, sk, A.data, BT.data, C.data);
+      }
+}
+
+int main(){
+  int n = MATRIX_SIZE;
+  char filenameA[256], filenameB[256];
+  snprintf(filenameA, sizeof(filenameA), "./datasets/matrix_%dx%d_A.bin", n, n);
+  snprintf(filenameB, sizeof(filenameB), "./datasets/matrix_%dx%d_B.bin", n, n);
+  
+  FILE* fileA = fopen(filenameA, "rb");
+  FILE* fileB = fopen(filenameB, "rb");
+
+  matrix A = read_matrix(fileA);
+  matrix B = read_matrix(fileB);
+  matrix BT = transpose_matrix(B);
+ 
+  matrix C;
+  C.rows = A.rows;
+  C.cols = A.cols;
+  C.data = calloc(C.rows * C.cols, sizeof(double));
+
+  dgemm_args args = { .A = A, .BT = BT, .C = C };
+
+  double elapsed = benchmark(dgemm, &args);
+
+  //print_matrix(A);
+  //print_matrix(B);
+  //print_matrix(C);
+  printf("Execution time: %f seconds \n", elapsed);
+
+  release_matrix(A);
+  release_matrix(B);
+  release_matrix(C);
+
+  return 0;  
+}
+
+/*
+#include <x86intrin.h>
+#define UNROLL (4)
+#define BLOCKSIZE 32
+
+void do_block(int n, int si, int sj, int sk, double* A, double* B, double* C){
+  for(int i = si; i < si + BLOCKSIZE; i+=UNROLL*8){
+    for(int j = sj; i < sj + BLOCKSIZE; j++){
+      __m512d c[UNROLL];
+      for(int r = 0; r < UNROLL; r++)
+        c[r] = _mm512_load_pd(C+i+r*8+j*n); // [UNROLL];
+      
+      for(int k = sk; k < sk + BLOCKSIZE; k++){
+        __m512d bb = _mm512_broadcast_pd(_mm_load_sd(B+j*n+k));
+        for(int r = 0; r < UNROLL, r++)
+          c[r] = _mm512_fmadd_pd(_mm512_load_pd(A+n*k+r*8+i), bb, c[r]);
+      }
+
+      for(int r = 0; r < UNROLL; r++)
+        _mm512_store_pd(C+i+r*8+j*n, c[r]);
+    }
+  }
+}
+
+void dgemm(size_t n, double* A, double* B, double* C){
+#pragma omp parallel for
+  for(int sj = 0; sj < n; sj += BLOCKSIZE)
+    for(int si = 0; si < n; si += BLOCKSIZE)
+      for(int sk = 0; sk < n; sk += BLOCKSIZE)
+        do_block(n, si, sj, sk, A, B, C);
+}
+*/
